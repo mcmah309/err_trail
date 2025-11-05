c8f9fb53f1fa9f6a531fffc6aeda93a766277e99
mcmah309 <dillonmcmahon79@gmail.com>
2025-11-05 04:46:33 +0000

    meta: Add no_std test

---
+++ test_no_std/main.rs
@@ -0,0 +1,46 @@
+#![no_std]
+#![no_main]
+
+use err_trail::{ErrContext, ErrContextDisplay};
+use exit_no_std::exit;
+
+#[unsafe(no_mangle)]
+fn main() -> i32 {
+    exit(0);
+}
+
+// Purposely not called since then we would have to set up the logger. Just making sure it compiles.
+#[allow(dead_code)]
+fn log() {
+    let x: Result<u32, &str> = Err("error value");
+    let _: Result<u32, &str> = x.error("context around");
+    let _: Option<u32> = x.consume_info();
+}
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    exit(1);
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn rust_eh_personality() {
+    exit(2);
+}
+
+#[derive(Debug)]
+pub struct TestError(u32);
+
+impl TestError {
+    pub fn new(code: u32) -> Self {
+        Self(code)
+    }
+}
+
+impl core::error::Error for TestError {}
+
+impl core::fmt::Display for TestError {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        write!(f, "TestError: {}", self.0)
+    }
+}
